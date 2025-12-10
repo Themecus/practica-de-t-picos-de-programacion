be77b8828dcf7783721ac4821b34a20a8d705461
Themecus <127447399+Themecus@users.noreply.github.com>
2025-12-10 10:12:43 -0400

    quede en el pasom 15

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import cors from 'cors';
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 
 const app = express()
+dotenv.config()
+
+const connectDB=()=>{
+    const {
+        MONGO_USERNAME,
+        MONGO_PASSWORD,
+        MONGO_PORT,
+        MONGO_DB,
+        MONGO_HOSTNAME,
+    }=process.env
+
+//agregar metodo patch
+
+    const url= `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`
+
+    mongoose.connect(url).then(function(){
+        console.log('mongoDB is connected')
+    })
+    .catch(function(err){
+        console.log(err)
+    })
+}
+
 
 const port = 3005
 app.use(cors({ origin: '*' })) 
@@ -9,10 +35,11 @@ app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: false }))
 
 app.listen(port, function () {
+    connectDB()
     console.log(`Api corriendo en http://localhost:${port}!`)
 })
 
 app.get('/', (req, res)=> {
     console.log('mi primer endponit')
-    res.status(200).send('hola la api funciona bien!')
+    res.status(200).send('hola la api funciona bien Canache!')
 })
